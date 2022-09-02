4bffa8238e762ea4a1926288ed4942559720b605
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-02 16:00:17 -0300

    alteração

--- Java/primeiroProjeto/src/metodoPt2.java
+++
@@ -1,9 +0,0 @@
-public class metodoPt2 {
-    public static String contador(int i, int f) {
-        String s = "";
-        for (int c = i; c <= f; c++) {
-            s += c + " ";
-        }
-        return s;
-    }
-}
